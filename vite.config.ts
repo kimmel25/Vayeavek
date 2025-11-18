8b5111f30ae028d30113562053dc90c73bfa1707
kimmel25 <dovikimmel@gmail.com>
2025-11-18 14:00:25 -0500

    working on it...

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",        // added from gpt
 })
+
+//this is imortant for my page
